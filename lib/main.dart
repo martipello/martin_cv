e94e17a28b0651887ed610801a825dd52fad2c71
martipello <martinseal1987@gmail.com>
2024-08-23 13:06:29 +0100

    ADD animating logo nested scroll etc

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:martin_cv/extensions/media_query_context_extension.dart';
-import 'package:martin_cv/margins.dart';
 import 'package:martin_cv/navigation_config.dart';
 import 'package:martin_cv/theme/theme.g.dart';
 
@@ -35,12 +34,61 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
       drawer: const MartinsDrawer(),
-      body: Column(
-        children: <Widget>[
-          _buildHeader(),
-        ],
+      body: NestedScrollView(
+        headerSliverBuilder: (nestedContext, innerScroll) {
+          return [
+            _buildSliverHeader(nestedContext),
+          ];
+        },
+        body: Builder(
+          builder: (context) {
+            return CustomScrollView(
+              slivers: [
+                SliverOverlapInjector(
+                  handle: NestedScrollView.sliverOverlapAbsorberHandleFor(context),
+                ),
+
+                SliverList(
+                  delegate: SliverChildListDelegate(
+                    List.generate(
+                      20,
+                      (index) {
+                        return Text('List item $index');
+                      },
+                    ),
+                  ),
+                ),
+              ],
+            );
+          },
+        ),
+      ),
+    );
+  }
+
+  Widget _buildSliverHeader(BuildContext nestedContext) {
+    final isLargeScreen = MediaQuery.of(context).isLargeScreen;
+    final headerHeight = isLargeScreen ? 400.0 : 200.0;
+    final logoHeight = isLargeScreen ? 250.0 : 100.0;
+    return SliverOverlapAbsorber(
+      handle: NestedScrollView.sliverOverlapAbsorberHandleFor(nestedContext),
+      sliver: SliverAppBar(
+        pinned: true,
+        expandedHeight: headerHeight,
+        flexibleSpace: FlexibleSpaceBar(
+          title: SafeArea(
+            child: Align(
+              alignment: Alignment.bottomRight,
+              child: Image.asset(
+                'assets/images/seal_studios_logo.png',
+                height: logoHeight,
+              ),
+            ),
+          ),
+          titlePadding: const EdgeInsets.all( 8),
+          background: _buildHeader(),
+        ),
       ),
     );
   }
@@ -52,27 +100,11 @@ class _MyHomePageState extends State<MyHomePage> {
       constraints: BoxConstraints.expand(
         height: headerHeight,
       ),
-      child: Stack(
-        alignment: AlignmentDirectional.topCenter,
-        children: [
-          OverflowBox(
-            child: Image.asset(
-              width: double.infinity,
-              height: headerHeight,
-              'assets/images/header_image.jpg',
-              fit: BoxFit.cover,
-            ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              Image.asset(
-                'assets/images/seal_studios_logo.png',
-              ),
-              kXLargeMargin,
-            ],
-          ),
-        ],
+      child: Image.asset(
+        width: double.infinity,
+        height: headerHeight,
+        'assets/images/header_image.jpg',
+        fit: BoxFit.cover,
       ),
     );
   }
